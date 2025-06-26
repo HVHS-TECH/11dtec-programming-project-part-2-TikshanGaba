55e241018cc07d580701fe38416f4acddb6f6b78
TikshanGaba <25371tg@hvhs.school.nz>
2025-06-26 01:16:11 +0000

    Jun 26, 2025, 1:16 AM

--- code.js
+++ code.js
@@ -28,29 +28,5 @@ Functions
 ****************************/
  function start()
  { 
-   userName = prompt("What is your name ?")
- if ( !isNaN(userName) || userName == null || userName == "" || userName == " " ){
-    userName = prompt("Invalid! You must enter a name")
-    // check if user has clicked cancel
- if (userName == null){
-   // end the program
-   return;
+   <input type="text" id="input_name" required></input>
  }
-}
-   userAge = prompt("Nice name " + userName +" How old are you ?")
- if (userAge == null || userAge == "" || userAge == " " || isNaN(userAge) || userAge < MINAGE || userAge > MAXAGE){
-    userAge = prompt("Invalid! You must enter you age between " + MINAGE + " and " + MAXAGE)
-    if (userAge == null){
-   // end the program
-   return;
- }
-}
-Seats = prompt("How much seats do you require in your car ?")
- if (Seats == null || Seats == "" || Seats == " " || isNaN(Seats) || Seats < MINSEATS || Seats > MAXSEATS){
-  Seats = prompt("Invalid! You must enter seats between" + MINSEATS + " and " + MAXSEATS)
-  if (Seats == null){
-    // end the program
-    return;
-  }
- }
-}
